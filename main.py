a0f0b1b50107df8aec60809b2b5bfe294245affe
denysyakymchuk <yakymchukdenys45@gmail.com>
2023-01-04 01:50:20 +0100

    Save

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
-import aiohttp
-session = aiohttp.ClientSession()
 #use the session here
 
 
@@ -18,11 +16,13 @@ chat_id = -746607958
 
 
 async def funcname():
-    l = await bot.get_chat(chat_id)
+    l = await bot.get_chat_members_count(chat_id)
     print(l)
 
 #JBC3ux5knZ4j9n2
 
 
 
-asyncio.run(funcname())
+if __name__ == '__main__':
+    asyncio.run(funcname())
+    executor.start_polling(dp, skip_updates=True)
